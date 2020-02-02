0bcc434bb85b574e65ab0a3bc98bee8fa97800d7
Juanse Gómez <js.gomez14@uniandes.edu.co>
2020-02-02 14:47:35 -0500

    Pattern Matching

--- src/exercises/MyGenericList.scala
+++ src/exercises/MyGenericList.scala
@@ -3,7 +3,7 @@ package exercises
 /**
  * Created by Daniel.
  */
-abstract class MyList[+A] {
+abstract class MyGenericList[+A] {
 
   /*
      head = first element of  the  list
@@ -14,55 +14,55 @@ abstract class MyList[+A] {
    */
 
   def head: A
-  def tail: MyList[A]
+  def tail: MyGenericList[A]
   def isEmpty: Boolean
-  def add[B >: A](element: B): MyList[B]
+  def add[B >: A](element: B): MyGenericList[B]
   def printElements: String
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 
   // higher-order functions
-  def map[B](transformer: A => B): MyList[B]
-  def flatMap[B](transformer: A => MyList[B]): MyList[B]
-  def filter(predicate: A => Boolean): MyList[A]
+  def map[B](transformer: A => B): MyGenericList[B]
+  def flatMap[B](transformer: A => MyGenericList[B]): MyGenericList[B]
+  def filter(predicate: A => Boolean): MyGenericList[A]
 
   // concatenation
-  def ++[B >: A](list: MyList[B]): MyList[B]
+  def ++[B >: A](list: MyGenericList[B]): MyGenericList[B]
 
   // hofs
   def foreach(f: A => Unit): Unit
-  def sort(compare: (A, A) => Int): MyList[A]
-  def zipWith[B, C](list: MyList[B], zip:(A, B) => C): MyList[C]
+  def sort(compare: (A, A) => Int): MyGenericList[A]
+  def zipWith[B, C](list: MyGenericList[B], zip:(A, B) => C): MyGenericList[C]
   def fold[B](start: B)(operator: (B, A) => B): B
 }
 
-case object Empty extends MyList[Nothing] {
+case object GenericEmpty extends MyGenericList[Nothing] {
   def head: Nothing = throw new NoSuchElementException
-  def tail: MyList[Nothing] = throw new NoSuchElementException
+  def tail: MyGenericList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyGenericList[B] = new GenericCons(element, GenericEmpty)
   def printElements: String = ""
 
-  def map[B](transformer: Nothing => B): MyList[B] = Empty
-  def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
-  def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
+  def map[B](transformer: Nothing => B): MyGenericList[B] = GenericEmpty
+  def flatMap[B](transformer: Nothing => MyGenericList[B]): MyGenericList[B] = GenericEmpty
+  def filter(predicate: Nothing => Boolean): MyGenericList[Nothing] = GenericEmpty
 
-  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+  def ++[B >: Nothing](list: MyGenericList[B]): MyGenericList[B] = list
 
   // hofs
   def foreach(f: Nothing => Unit): Unit = ()
-  def sort(compare: (Nothing, Nothing) => Int) = Empty
-  def zipWith[B, C](list: MyList[B], zip: (Nothing, B) => C): MyList[C] =
+  def sort(compare: (Nothing, Nothing) => Int) = GenericEmpty
+  def zipWith[B, C](list: MyGenericList[B], zip: (Nothing, B) => C): MyGenericList[C] =
     if (!list.isEmpty) throw new RuntimeException("Lists do not have the same length")
-    else Empty
+    else GenericEmpty
   def fold[B](start: B)(operator: (B, Nothing) => B): B = start
 }
 
-case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+case class GenericCons[+A](h: A, t: MyGenericList[A]) extends MyGenericList[A] {
   def head: A = h
-  def tail: MyList[A] = t
+  def tail: MyGenericList[A] = t
   def isEmpty: Boolean = false
-  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
+  def add[B >: A](element: B): MyGenericList[B] = new GenericCons(element, this)
   def printElements: String =
     if(t.isEmpty) "" + h
     else h + " " + t.printElements
@@ -74,8 +74,8 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
       = new Cons(2, Empty.filter(n % 2 == 0))
       = new Cons(2, Empty)
    */
-  def filter(predicate: A => Boolean): MyList[A] =
-    if (predicate(h)) new Cons(h, t.filter(predicate))
+  def filter(predicate: A => Boolean): MyGenericList[A] =
+    if (predicate(h)) new GenericCons(h, t.filter(predicate))
     else t.filter(predicate)
 
   /*
@@ -85,8 +85,8 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
       = new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
       = new Cons(2, new Cons(4, new Cons(6, Empty))))
    */
-  def map[B](transformer: A => B): MyList[B] =
-    new Cons(transformer(h), t.map(transformer))
+  def map[B](transformer: A => B): MyGenericList[B] =
+    new GenericCons(transformer(h), t.map(transformer))
 
   /*
     [1,2] ++ [3,4,5]
@@ -94,7 +94,7 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     = new Cons(1, new Cons(2, Empty ++ [3,4,5]))
     = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5)))))
    */
-  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
+  def ++[B >: A](list: MyGenericList[B]): MyGenericList[B] = new GenericCons(h, t ++ list)
   /*
     [1,2].flatMap(n => [n, n+1])
     = [1,2] ++ [2].flatMap(n => [n, n+1])
@@ -102,7 +102,7 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     = [1,2] ++ [2,3] ++ Empty
     = [1,2,2,3]
    */
-  def flatMap[B](transformer: A => MyList[B]): MyList[B] =
+  def flatMap[B](transformer: A => MyGenericList[B]): MyGenericList[B] =
     transformer(h) ++ t.flatMap(transformer)
 
   // hofs
@@ -111,19 +111,19 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     t.foreach(f)
   }
 
-  def sort(compare: (A, A) => Int): MyList[A] = {
-    def insert(x: A, sortedList: MyList[A]): MyList[A] =
-      if (sortedList.isEmpty) new Cons(x, Empty)
-      else if (compare(x, sortedList.head) <= 0) new Cons(x, sortedList)
-      else new Cons(sortedList.head, insert(x, sortedList.tail))
+  def sort(compare: (A, A) => Int): MyGenericList[A] = {
+    def insert(x: A, sortedList: MyGenericList[A]): MyGenericList[A] =
+      if (sortedList.isEmpty) new GenericCons(x, GenericEmpty)
+      else if (compare(x, sortedList.head) <= 0) new GenericCons(x, sortedList)
+      else new GenericCons(sortedList.head, insert(x, sortedList.tail))
 
     val sortedTail = t.sort(compare)
     insert(h, sortedTail)
   }
 
-  def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C] =
+  def zipWith[B, C](list: MyGenericList[B], zip: (A, B) => C): MyGenericList[C] =
     if (list.isEmpty) throw new RuntimeException("Lists do not have the same length")
-    else new Cons(zip(h, list.head), t.zipWith(list.tail, zip))
+    else new GenericCons(zip(h, list.head), t.zipWith(list.tail, zip))
 
   /*
     [1,2,3].fold(0)(+) =
@@ -137,11 +137,11 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
 }
 
-object ListTest extends App {
-  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
-  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+object GenericListTest extends App {
+  val listOfIntegers: MyGenericList[Int] = new GenericCons(1, new GenericCons(2, new GenericCons(3, GenericEmpty)))
+  val cloneListOfIntegers: MyGenericList[Int] = new GenericCons(1, new GenericCons(2, new GenericCons(3, GenericEmpty)))
+  val anotherListOfIntegers: MyGenericList[Int] = new GenericCons(4, new GenericCons(5, GenericEmpty))
+  val listOfStrings: MyGenericList[String] = new GenericCons("Hello", new GenericCons("Scala", GenericEmpty))
 
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
@@ -151,7 +151,7 @@ object ListTest extends App {
   println(listOfIntegers.filter(_ % 2 == 0).toString)
 
   println((listOfIntegers ++ anotherListOfIntegers).toString)
-  println(listOfIntegers.flatMap(elem => new Cons(elem, new Cons(elem + 1, Empty))).toString)
+  println(listOfIntegers.flatMap(elem => new GenericCons(elem, new GenericCons(elem + 1, GenericEmpty))).toString)
 
   println(cloneListOfIntegers == listOfIntegers)
 
